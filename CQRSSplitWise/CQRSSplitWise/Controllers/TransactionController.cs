7ac53d7805202af203e1cb7faf5eb7428e3e07b0
Domagoj Hrestak <domagoj.hrestak@lemax.net>
2020-10-15 16:51:44 +0200

    Initial commit, gitignore, structure, MediatR added

---
+++ CQRSSplitWise/CQRSSplitWise/Controllers/TransactionController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CQRSSplitWise.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CQRSSplitWise.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class TransactionController : ControllerBase
+	{
+		private readonly ILogger<TransactionController> _logger;
+
+		public TransactionController(ILogger<TransactionController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpPost]
+		public async Task<Transaction> Add()
+		{
+			return new Transaction();
+		}
+	}
+}
