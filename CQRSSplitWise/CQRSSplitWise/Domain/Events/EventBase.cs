47419b5f3fbc1172977d89b9a49e4953d421f63e
Mario Čale <mario.cale@lemax.net>
2020-11-13 09:43:56 +0100

    Add EventBase and TransactionEvent classes

---
+++ CQRSSplitWise/CQRSSplitWise/Domain/Events/EventBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRSSplitWise.Domain.Events
+{
+	public abstract class EventBase
+	{
+		protected Guid EventID { get; set; }
+		protected string EventType { get; set; }
+		protected string EventData { get; set; }
+		protected string EventMetadata { get; set; }
+	}
+}
---
+++ CQRSSplitWise/CQRSSplitWise/Domain/Events/TransactionEvent.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRSSplitWise.Domain.Events
+{
+	public class TransactionEvent : EventBase
+	{
+		private readonly string _eventType = "TransactionCreated";
+
+		public TransactionEvent(object eventData, object eventMetadata)
+		{
+			EventID = Guid.NewGuid();
+			EventType = _eventType;
+
+			EventData = JsonConvert.SerializeObject(eventData, Formatting.Indented);
+			EventMetadata = JsonConvert.SerializeObject(eventMetadata, Formatting.Indented);
+		}
+	}
+}
